de84addde1b0c567b0f01c696effb0030a198922
Oguzhnblt <oguzhanbolat0@gmail.com>
2023-08-16 19:58:16 +0300

    Initial Commit

---
+++ Network Interchangable/Network_InterchangableApp.swift
@@ -0,0 +1,17 @@
+//
+//  Network_InterchangableApp.swift
+//  Network Interchangable
+//
+//  Created by Oğuz on 16.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct Network_InterchangableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
